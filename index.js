b1e9c9be929d717b731b52150966fd4eb3eba7d7
Eugemar Jiménez <122749830+TheGirlEu@users.noreply.github.com>
2023-09-23 17:10:29 -0500

    variables en .js para cada form

---
+++ index.js
@@ -0,0 +1,5 @@
+function validateForm() {
+    let x = document.forms["form"]["code"].value
+    let y = document.forms["form"]["telefono"].value
+    let z = document.forms["form"]["mensaje"].value
+}
